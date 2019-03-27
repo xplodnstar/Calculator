d07474ffd338b86fa89706af0147b050b3d0111d
Nova <xplodnstar@live.com>
2019-03-27 15:28:39 -0700

    final commit of explore mode

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
 $(document).ready(function () {
 
+    $("#add").on('click', function (e) {
+        e.preventDefault()
 
+        var num1 = Number($("#num1").val())
+        var num2 = Number($("#num2").val())
+
+        $("#answer").html(num1 + num2)
+
+    })
+
+    $("#sub").on('click', function (e) {
+        e.preventDefault()
+
+        var num1 = Number($("#num1").val())
+        var num2 = Number($("#num2").val())
+
+        $("#answer").html(num1 - num2)
+    })
+
+    $("#mult").on('click', function (e) {
+        e.preventDefault()
+
+        var num1 = Number($("#num1").val())
+        var num2 = Number($("#num2").val())
+
+        $("#answer").html(num1 * num2)
+    })
+
+    $("#divd").on('click', function (e) {
+        e.preventDefault()
+
+        var num1 = Number($("#num1").val())
+        var num2 = Number($("#num2").val())
+
+        $("#answer").html(num1 / num2)
+    })
 
 
 })
